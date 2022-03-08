357b0909d6a8b44d5a4a4c5e25216ca3e3e25f59
mikeqg <mike.gu868@outlook.com>
2022-03-08 13:52:20 +0200

    append data to local DB

--- DBtiedonlisays.py
+++ DBtiedonlisays.py
@@ -1,31 +1,29 @@
 import psycopg2
 
-def append_db(localfilename,container,blob,URL,cursor):
-    SQL = "INSERT INTO blob1 (localfilename, BLOB_CONTAINER, blob_name, containerURL) VALUES (%s,%s,%s,%s);"
-    data = (localfilename,container,blob,URL)
+def append_db(nimi,alku,loppu,projekti_nimi,selite,cursor):
+    SQL = "INSERT INTO tyo_taulu (nimi,alku,loppu,projekti_nimi,selite) VALUES (%s,%s,%s,%s,%s);"
+    data = (nimi,alku,loppu,projekti_nimi,selite)
     cursor.execute(SQL,data)
+
 
 if __name__ == '__main__':
 
     with open("./ignore.txt", 'r') as file:
         lines = [line.rstrip() for line in file]
-    cstr = lines[0]
-    pw = lines[1]
+    pw = lines[0]
 
-    storage_name = "mikestorageacademy"
-    BLOB_CONTAINER = "mikecontainer"
-    blob_name = "picture.png"
-    localfilename = "./src/picture.png"
-    containerURL = "https://"+storage_name+".blob.core.windows.net/"+BLOB_CONTAINER
-
-    #uploadfile(BLOB_CONTAINER,blob_name,cstr,localfilename)
+    nimi = 'matti'
+    alku = '2011-10-16 15:36:38'
+    loppu = '2011-10-22 15:36:38'
+    projekti_nimi = 'blabla'
+    selite = 'blablabla'
 
     con = None
     try:
-        con = psycopg2.connect("dbname=blobdb user=postgres password = {}".format(pw))
+        con = psycopg2.connect("dbname=tyotunnit user=postgres password = {}".format(pw))
         cursor = con.cursor()
 
-        append_db(localfilename,BLOB_CONTAINER,blob_name,containerURL,cursor)
+        append_db(nimi,alku,loppu,projekti_nimi,selite,cursor)
 
         con.commit()
         cursor.close()
