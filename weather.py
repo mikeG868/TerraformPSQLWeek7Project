f8d3e0d4e1c444b303a6a994347f58bde87141d4
mikeqg <mike.gu868@outlook.com>
2022-03-09 17:16:15 +0200

    weather function

--- weather.py
+++ weather.py
@@ -1,13 +1,18 @@
 import requests
 
-#Db connection info
-with open("./DBignore.txt", 'r') as file:
-    lines = [line.rstrip() for line in file]
-apikey = lines[3]
+def get_weather(apikey,city):
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&appid={}&units=metric'.format(city,apikey)
+    res = requests.get(url)
+    data = res.json()
+    return data['main']['temp']
 
-city = input('Enter your city : ')
-url = 'http://api.openweathermap.org/data/2.5/weather?q={}&appid={}&units=metric'.format(city,apikey)
-res = requests.get(url)
-data = res.json()
-temp = data['main']['temp']
-print('Temperature : {} degree celcius at {}'.format(temp,city))
+
+if __name__ == '__main__':
+    #API key
+    with open("./DBignore.txt", 'r') as file:
+        lines = [line.rstrip() for line in file]
+    apikey = lines[3]
+
+    city = 'helsinki'
+    temp = get_weather(apikey,city)
+    print('Temperature : {} degree celcius at {}'.format(temp,city))
